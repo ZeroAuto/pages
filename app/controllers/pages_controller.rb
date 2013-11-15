3d6f82c8733966eb45b40c1212cd74a1614a9fe4
Mike Cooley <mike@jackis-mac-mini.gateway.2wire.net>
2013-11-14 19:50:05 -0600

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+
+	def index
+	end
+
+	def okgamer
+	end
+	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySite::Application.config.secret_key_base = 'e7808751dbd324506173d0a90ef9422ebdd0de8215d6b2f1e3abe99b7dde1cd45d2451d99d67606b899cd647558e187a8dffa5c3e8062e1d11ce05405a751ced'
